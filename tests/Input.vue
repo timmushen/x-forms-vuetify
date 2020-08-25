9027c77e3f4427b397ab67b848846953d782f646
Tim Mushen <t@getx.io>
2020-08-24 18:06:06 -0700

    input

---
+++ tests/Input.vue
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { mount, createLocalVue } from '@vue/test-utils'
+
+import Input from '@/components/Input.vue'
+
+describe('Input', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Input)
+    expect(wrapper).toBeTruthy()
+  })
+})
